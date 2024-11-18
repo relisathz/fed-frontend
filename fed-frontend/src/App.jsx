15d4210ceee75916012ab0b36c5d531694da71bc
relisathz <dula98sathsarani@gmail.com>
2024-11-18 11:16:06 +0530

    initial commit

---
+++ fed-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Greeting from './Greeting'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+     <Greeting/>
+    </div>
+  )
+}
+
+export default App
---
+++ fed-frontend/src/Greeting.jsx
@@ -0,0 +1,10 @@
+function Greeting() {
+    const name="dula";
+    return ( 
+    <div>
+        <h1>Hello {name}</h1>
+    </div> );
+    
+}
+
+export default Greeting;
